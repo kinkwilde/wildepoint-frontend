364729681fa0574ae67c4fcc4dfdd4c641988986
Stephen Wilde <kink@mandys.house>
2022-06-03 03:33:46 +0100

    update dark bg

--- components/templates/index/index.js
+++ components/templates/index/index.js
@@ -3,9 +3,9 @@ import ThemeOrganismsFooterSplash from "../../theme/organisms/footer/splash";
 
 export default function TemplateIndex({ children }) {
     return (
-        <div className="bg-white p-2 dark:bg-black sm:p-4 md:p-6 lg:p-12 xl:p-24">
+        <div className="bg-grey p-2 dark:bg-black sm:p-4 md:p-6 lg:p-12 xl:p-24">
             <div className="container">
-                <div className="rounded-lg border-4 border-black p-2 dark:border-white sm:p-4 md:p-6 lg:p-12 xl:p-24">
+                <div className="rounded-lg border-4 border-black bg-white p-2 dark:border-white dark:bg-gray-900 sm:p-4 md:p-6 lg:p-12 xl:p-24">
                     <div className="flex flex-col">
                         <ThemeOrganismsHeaderSplash />
                         <main className="grow">{children}</main>
